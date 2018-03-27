96404832d474df8c98ae39a5fee267cb209479ba
jayala-29 <jayala@ucsd.edu>
2018-03-27 15:34:05 -0700

    Update bisection.py

--- bisection.py
+++ bisection.py
@@ -8,11 +8,13 @@ def bisection(f, a, b):
   c = (a + b) / 2
   fa = evaluate(f, a)
   fc = evaluate(f, c)
-  if (abs(fa * fc) == 0):
+  if (abs(fa * fc) < 1):
     return c
   elif (fa * fc < 0):
     return bisection(f, a, c)
-  return bisection(f, c, b)
+  elif (fa * fc < 0):
+    return bisection(f, c, b)
+  return "DNE"
   
 # computes bisection on x^-1 - arctan(x) on the interval [0, pi/2]
 print (bisection("(1 / x) - m.atan(x)", 0 + 0.000001, m.pi / 2 - 0.000001))
